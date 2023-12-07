df293177201ad3fe7f8498aba10aec1afa43e03e
danericp <daneric.pelayo@gmail.com>
2023-12-07 00:14:34 -0500

    Working version

--- midge/action/fetch.py
+++ midge/action/fetch.py
@@ -1,26 +1,47 @@
 
 import os
+import shutil
 
 str_folder_destination = r'C:\Users\daner\Downloads'
 str_folder_prefix = 'Files - '
-str_folder_source = r'C:\Users\daner\Documents\test'
+str_folder_source = r'C:\Users\daner\Downloads'
+str_protocol_ifexist = 'rename'
+
+def do_move(target, destination):
+    try:
+        os.makedirs(destination)
+    except FileExistsError:
+        print("Folder already exists: " + destination)
+    except OSError as e:
+        print("Failed to create folder " + destination + ": " + e)
+        exit()
+    finally:
+        if str_protocol_ifexist == 'overwrite':
+            shutil.move(target, destination)
+        elif str_protocol_ifexist == 'rename':
+            int_counter = 1
+            destination_new = destination + '\\' + os.path.basename(target)
+            while os.path.exists(destination_new):
+                int_counter = int_counter + 1
+                destination_new = os.path.splitext(destination_new)[0] + " Copy " + str(int_counter) + os.path.splitext(destination_new)[1]
+            shutil.move(target, destination_new)
+
+def do_move_check(target, destination):
+    boolean_proceed = True
+    if os.path.isfile(target):
+        str_folder_destination_new = destination + '\\' + str_folder_prefix + os.path.splitext(str_entry)[1][1:].upper()
+    elif os.path.isdir(target):
+        str_folder_destination_new = destination + r'\Folder'
+        if os.path.basename(target) == 'Folder' or os.path.basename(target)[0:len(str_folder_prefix)] == str_folder_prefix:
+            print("Cannot proceed moving the folder '" + target)
+            boolean_proceed = False
+    if boolean_proceed:
+        do_move(target, str_folder_destination_new)
 
 try:
     for str_entry in os.listdir(str_folder_source):
         str_entry_abs = str_folder_source + '\\' + str_entry
-        if os.path.isfile(str_entry_abs):
-            str_ext = os.path.splitext(str_entry)[1][1:]
-            str_folder_destination_new = str_folder_destination + '\\' + str_folder_prefix + str_ext.upper()
-            try:
-                os.makedirs(str_folder_destination_new)
-            except FileExistsError:
-                print("Folder already exists: " + str_folder_destination_new)
-            except OSError as e:
-                print("Failed to create folder " + str_folder_destination_new + ": " + e)
-        elif os.path.isdir(str_entry_abs):
-            # Pending
-            pass
-        pass
+        do_move_check(str_entry_abs, str_folder_destination)
 except FileNotFoundError:
     print("Directory not found for " + str_folder_source)
 except PermissionError:
